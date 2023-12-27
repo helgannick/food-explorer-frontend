03787a03f7d9ef758c8dd4b001af903d8c98f7f3
helgannick <helgannick@gmail.com>
2023-12-27 14:33:03 -0300

    styles config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/global";
+import { AuthProvider } from "./hooks/auth"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import theme from "./styles/theme";
+
+import { Routes } from "./routes";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
+
+    </ThemeProvider>
+  </React.StrictMode>
+);
---
+++ src/styles/theme.js
@@ -0,0 +1,21 @@
+export default {
+  COLORS: {
+    BACKGROUND_400: "#000A0F",
+    BACKGROUND_900: "#0D1D25",
+    BACKGROUND_700: "#001119",
+
+    DARK_200: "#00070A",
+    DARk_600: "#00111A",
+    DARK_1000: "#192227",
+
+    LIGHT_100: "#FFFFFF",
+    LIGHT_300: "#E1E1E6",
+    LIGHT_400: "#C4C4CC",
+    LIGHT_500: "#7C7C8A",
+
+    CAKE_100: "#065E7C",
+    CAKE_200: "#82F3FF",
+
+    TOMATO_100: "#750310"
+  }
+}
